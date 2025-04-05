c1f406306f362479cccb270cce81574ff4d65814
Connor Hall <connrhall204@gmail.com>
2025-04-05 14:43:58 -0500

    Added and styled email form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const { log } = console;
+
+import * as utils from './utils.js';
+
+const name = utils.select('.name');
+const email = utils.select('.email');
+const message = utils.select('.message');
+
+// If I wanted to fail I'd find a way to use this...
+
+// // utils.js
+// export async function fetchRandomQuote() {
+//     const response = await fetch("https://api.quotable.io/random");
+//     const data = await response.json();
+//     return data.content;
+// }
